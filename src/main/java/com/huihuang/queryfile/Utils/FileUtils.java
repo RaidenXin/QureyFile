70850ddaf53c26eb8f1b22fbc9918e74abe3c0e7
Raiden <xinlei19900727@outlook.com>
2018-12-04 22:34:30 +0800

    新增Excel支持

--- src/main/java/com/huihuang/queryfile/Utils/FileUtils.java
+++ src/main/java/com/huihuang/queryfile/Utils/FileUtils.java
@@ -4,11 +4,23 @@ import com.huihuang.queryfile.QueryFileProcessor;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.InputStream;
 import java.nio.ByteBuffer;
 import java.nio.channels.FileChannel;
 import java.nio.charset.Charset;
 
+import org.apache.poi.xssf.usermodel.XSSFCell;
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
 public class FileUtils {
+
+    private static final String EXCEL2007 = ".xlsx";
+    private static final String EXCEL2003 = ".xls";
+    private static final String HTML = ".html";
+    private static final String TXT = ".txt";
+
     /**
      * *解析文件返回文件内容字符串
      * @param file
@@ -17,6 +29,23 @@ public class FileUtils {
      */
     @SuppressWarnings("resource")
     public static  String fileParse(File file, String endFileName) {
+        if (file.isFile()){
+            if(EXCEL2003.equals(endFileName) || EXCEL2007.equals(endFileName)){
+                return parseExcel(file,endFileName);
+            }else if (HTML.equals(endFileName) || TXT.equals(endFileName)){
+                return parseHtmlAndTxt(file,endFileName);
+            }
+        }
+        return StringUtils.EMPTY;
+    }
+
+    /**
+     * 解析页面或者文本文件
+     * @param file
+     * @param endFileName
+     * @return
+     */
+    private static String parseHtmlAndTxt(File file, String endFileName){
         String fileName = file.getName();
         ByteBuffer buffer = ByteBuffer.allocate(QueryFileProcessor.SIZE);
         StringBuffer stringBuffer = new StringBuffer(QueryFileProcessor.ENPTY_STR);
@@ -34,4 +63,55 @@ public class FileUtils {
         }
         return stringBuffer.toString();
     }
+
+    /**
+     * 解析Excel
+     * @param file
+     * @param endFileName
+     * @return
+     */
+    private static String parseExcel(File file, String endFileName){
+        StringBuffer stringBuffer = new StringBuffer(QueryFileProcessor.ENPTY_STR);
+        try (InputStream is = new FileInputStream(file);) {
+            XSSFWorkbook xssfWorkbook = new XSSFWorkbook(is);
+            for (int numSheet = 0,n = xssfWorkbook.getNumberOfSheets(); numSheet < n; numSheet++) {
+                XSSFSheet xssfSheet = xssfWorkbook.getSheetAt(numSheet);
+                if (null == xssfSheet){
+                    continue;
+                }
+                for (int j = 0, x = xssfSheet.getLastRowNum() + 1; j < x;j++){
+                    XSSFRow row = xssfSheet.getRow(j);
+                    stringBuffer.append(getValue(row));
+                }
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return stringBuffer.toString();
+    }
+
+    /**
+     * 获取一行的所有字符
+     * @param row
+     * @return
+     */
+    private static String getValue(XSSFRow row) {
+        StringBuilder builder = new StringBuilder(StringUtils.EMPTY);
+        if (null != row){
+            for (int i = 0,n = row.getLastCellNum(); i < n; i++){
+                XSSFCell xssfCell = row.getCell(i);
+                if (xssfCell.getCellType() == xssfCell.CELL_TYPE_BOOLEAN) {
+                    builder.append(String.valueOf(xssfCell.getBooleanCellValue()));
+                } else if (xssfCell.getCellType() == xssfCell.CELL_TYPE_NUMERIC) {
+                    System.err.println(xssfCell.getNumericCellValue());
+                    builder.append(String.valueOf(xssfCell.getNumericCellValue()));
+                } else {
+                    builder.append(String.valueOf(xssfCell.getStringCellValue()));
+                }
+                builder.append(";");
+            }
+        }
+        return builder.toString();
+    }
+
 }
