d1bf991d06d88ad17ad479b9f3f595c39516eb7a
Raiden <xinlei19900727@outlook.com>
2018-12-05 20:31:54 +0800

    字符串工具

---
+++ src/main/java/com/huihuang/queryfile/Utils/StringUtils.java
@@ -0,0 +1,13 @@
+package com.huihuang.queryfile.Utils;
+
+public class StringUtils {
+
+    public static final String EMPTY = "";
+
+    public static boolean isBlank(String value){
+        if (null == value || EMPTY.equals(value)){
+            return true;
+        }
+        return false;
+    }
+}
