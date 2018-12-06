bf364b41bb94860d00300ff4b5bc539385e4d4c5
Raiden <xinlei@powere2e.comgit config --global user.email xinlei@powere2e.com>
2018-12-06 15:50:17 +0800

    格式化了部分代码

--- src/main/java/com/huihuang/queryfile/TextArea.java
+++ src/main/java/com/huihuang/queryfile/TextArea.java
@@ -12,8 +12,6 @@ import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
-import javax.swing.event.AncestorEvent;
-import javax.swing.event.AncestorListener;
 
 public class TextArea extends JFrame{
 
